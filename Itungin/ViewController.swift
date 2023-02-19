faf153b437f49af234f2ce4669340c3a76c27b36
rivaldofez <rivaldofez@gmail.com>
2023-02-19 20:43:02 +0700

    init project

---
+++ Itungin/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Itungin
+//
+//  Created by Rivaldo Fernandes on 19/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
