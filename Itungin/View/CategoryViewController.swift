30f9219cf0ac7e8b21581f082cf51a080a9c9b87
rivaldofez <rivaldofez@gmail.com>
2023-02-25 15:47:17 +0700

    add title and close button in category sheet

--- Itungin/View/CategoryViewController.swift
+++ Itungin/View/CategoryViewController.swift
@@ -8,9 +8,59 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-
+    
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Select Category"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .label
+        
+        return label
+    }()
+    
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        
+        var configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(systemName: "xmark", withConfiguration: UIImage.SymbolConfiguration(pointSize: 10))
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        configuration.baseBackgroundColor = .lightGray.withAlphaComponent(0.2)
+        configuration.baseForegroundColor = .darkGray
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 12
+        button.clipsToBounds = true
+        button.configuration = configuration
+        
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(titleLabel)
+        view.addSubview(closeButton)
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        let titleLabelConstraints = [
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 16)
+        ]
+        
+        let closeButtonConstraints = [
+            closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            closeButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            closeButton.heightAnchor.constraint(equalToConstant: 24),
+            closeButton.widthAnchor.constraint(equalToConstant: 24)
+        ]
+        
+        NSLayoutConstraint.activate(titleLabelConstraints)
+        NSLayoutConstraint.activate(closeButtonConstraints)
     }
 }
