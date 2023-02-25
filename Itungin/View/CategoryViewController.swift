9ef0ca38118e6ba6f769630f2d3942ea7e472b25
rivaldofez <rivaldofez@gmail.com>
2023-02-25 15:11:59 +0700

    add category view controller

---
+++ Itungin/View/CategoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryViewController.swift
+//  Itungin
+//
+//  Created by Rivaldo Fernandes on 25/02/23.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
